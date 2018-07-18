af4f4875384b53c458b46542d90b7d9de9dacfbf
MapasLPRM <projetoufeslprm@gmail.com>
2018-07-18 17:26:17 -0300

    mudando link para planilha

--- site/js/exemplos.js
+++ site/js/exemplos.js
@@ -201,7 +201,7 @@ exemplo1 = function() {
 
 exemplo_gdoc = function() {
   var mps, public_spreadsheet_url;
-  public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/1_ypl-yywe33_R9W3nYW240TEmvoI0ErdHyYpS6tbuxo/edit?usp=sharing";
+  public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjLdpQ-d-lvX0kCpFhGgL01-XV_AuWbF9S_GWkWXsLUgQl9_qpHZg6Jd2jlKDGgmG9X1NlYc-hNFbl/pubhtml";
   mps = new Searchlight(public_spreadsheet_url, null, "map_gdoc", null, true, false);
 };
 
