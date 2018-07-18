7ed3844450281211927ecc634d98ac34e5f5c352
MapasLPRM <projetoufeslprm@gmail.com>
2018-07-18 15:40:55 -0300

    troca de planilha

--- site/js/exemplos.js
+++ site/js/exemplos.js
@@ -201,7 +201,7 @@ exemplo1 = function() {
 
 exemplo_gdoc = function() {
   var mps, public_spreadsheet_url;
-  public_spreadsheet_url = "https://docs.google.com/spreadsheet/pub?key=1JMRfKK2y56np1uPhz18RG3_GyVcZWp7Bx3VO6jd4fFw&single=true&gid=0&output=html";
+  public_spreadsheet_url = "https://docs.google.com/spreadsheet/pub?key=1_ypl-yywe33_R9W3nYW240TEmvoI0ErdHyYpS6tbuxo&single=true&gid=0&output=html";
   mps = new Searchlight(public_spreadsheet_url, null, "map_gdoc", null, true, false);
 };
 
